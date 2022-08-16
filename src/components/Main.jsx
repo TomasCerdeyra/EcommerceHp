d0b72d23bbc7816711363c88903e4929287f988d
Tomas Cerdeyra <tomascerdeyraimport@gmail.com>
2022-08-16 12:57:42 -0300

    Menu E-COMMERCE

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+import '../styles/main.css'
+
+const Main = () => {
+    return (
+        <main>
+            <NavBar />
+        </main>
+    )
+}
+
+export default Main
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../styles/navBar.css'
+
+const NavBar = () => {
+    return (
+        <div className='seccion1'>
+            <div className='cont-h1'>
+                <h1>
+                    Articulos de Harry Potter:
+                </h1>
+            </div>
+            <aside className="aside-list">
+                <nav>
+                    <ul className="ul-list">
+                        <li>Varitas</li>
+                        <li>Libros</li>
+                        <li>Funko Pop</li>
+                        <li>Grageas</li>
+                        <li>Accesorios</li>
+                        <li>Indumentarias</li>
+                        <li>Figuras</li>
+                    </ul>
+                </nav>
+            </aside>
+        </div>
+    )
+}
+
+export default NavBar
